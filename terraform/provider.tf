598c774f184c80a45f0b8a5d728575c9b5a5b2d0
joindevopscloud <info@joindevops.com>
2026-07-14 19:39:19 +0530

    terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -9,7 +9,7 @@ terraform {
   # remote state storage location
   backend "s3" {
     bucket         = "joindvops-state-160885265516-us-east-1-an"
-    key            = "s3-demo.tfstatez"
+    key            = "s3-demo.tfstate"
     region         = "us-east-1"
     encrypt        = true
     use_lockfile   = true # Enables native S3 state locking (Terraform 1.10+)
